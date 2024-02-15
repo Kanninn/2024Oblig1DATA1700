fc5df0f4628bacd766c23e6b96d416610a0b95a8
Jona <143602718+Kanninn@users.noreply.github.com>
2024-02-15 21:57:58 +0100

    Oppdatert README og synkronisering av inteij filer

---
+++ src/test/java/org/example/oblig1dape1700/Oblig1Dape1700ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.oblig1dape1700;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oblig1Dape1700ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
